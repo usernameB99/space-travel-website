6b433fc2b40f0e042e65e211438f153d936a1d68
usernameB99 <kum-a-saufn@live.at>
2024-02-27 17:08:43 +0100

    update

--- script.js
+++ script.js
@@ -36,26 +36,22 @@ function changeTabFocus(e) {
     // change the tabindex of the current tab to -1
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight){
         tabs[tabFocus].setAttribute("tabindex", -1);
-    }
-
-    // if the right key is pushed, move to the next tab on the right
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
+        
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        }else if (e.keyCode === keydownLeft) {
+                tabFocus--;
+                if (tabFocus < 0) {
+                tabFocus = tabs.length -1;
+            }
         }
+        
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
     }
-
-    // if the left key is pushed, move to the next tab on the left
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length -1;
-        }
-    }
-
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
 }
 
 function changeTabPanel(e){
